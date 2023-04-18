5fa8c3f4bc71c26b1ee75c1e14a04b334c8da5ee
HeikoRibberink <heiko.ribberink@deblijdeaankomst.nl>
2023-04-18 13:50:23 +0200

    Updated month view to include day events.

--- src/frontend/pages/month.rs
+++ src/frontend/pages/month.rs
@@ -2,26 +2,35 @@ use leptos::*;
 
 #[component]
 pub fn MonthView(cx: Scope) -> impl IntoView {
-	let a = (1..31)
+	let days_fill = (1..=31)
 		.into_iter()
 		.map(|n| view! {cx,
-			<MonthDay datum=n/>
+			<Day day=n/>
 		})
 		.collect::<Vec<_>>();
 	view! {cx,
 		<div class="month-view">
-			{a}
+			{days_fill}
 		</div>
 	}
 }
 
 #[component]
-pub fn MonthDay(cx: Scope, datum: i32) -> impl IntoView {
+pub fn Day(cx: Scope, day: u8) -> impl IntoView {
+	let items_fill = (0..5).into_iter().map(|n| view!{cx, <DayEvent description={ format!("Fill text {n}")} color="#1E70F0".to_string()/> }).collect::<Vec<_>>();
 	view! {cx,
 		<div class="month-day">
-			<p class="month-day-datum">{datum}</p>
+			<p class="month-day-datum">{day}</p>
 			<div class="month-day-items-wrapper">
+				{items_fill}
 			</div>
 		</div>
+	}
+}
+
+#[component]
+pub fn DayEvent(cx: Scope, description: String, color: String) -> impl IntoView {
+	view!{cx, 
+		<p class="month-day-event" style=format!("background-color: {color}")>{description}</p>
 	}
 }
